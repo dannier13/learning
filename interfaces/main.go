f476252d4ca81396775e64959e9390b4bf13fb66
64228646 <dan@Dans-MacBook-Pro.local>
2020-11-08 20:22:14 +0100

    interface

---
+++ interfaces/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	var w Writer = ConsoleWriter{}
+	w.Write([]byte("Hello Go!"))
+
+	myInt := IntCounter(0)
+	var inc Incrementer = &myInt
+	for i := 0; i < 10; i++ {
+		fmt.Println(inc.Increment())
+	}
+}
+
+type Writer interface {
+	Write([]byte) (int, error)
+}
+
+type ConsoleWriter struct{}
+
+func (cw ConsoleWriter) Write(data []byte) (int, error) {
+	n, err := fmt.Println(string(data))
+	return n, err
+}
+
+type Incrementer interface {
+	Increment() int
+}
+
+type IntCounter int
+
+func (ic *IntCounter) Increment() int {
+	*ic++
+	return int(*ic)
+}
